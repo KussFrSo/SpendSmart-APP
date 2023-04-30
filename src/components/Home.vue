8759a57ba360d77679c0e914db37836d87e6ca28
KussFrSo <marcfranch.fh@gmail.com>
2023-04-30 18:42:27 +0200

    [Task] Layout Structure

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,10 +3,18 @@
     <template #header>
       <Header />
     </template>
+    <template #resume>
+      <Resume />
+    </template>
+    <template #movements>
+      <Movements />
+    </template>
   </Layout>
 </template>
 
 <script setup>
 import Header from "@/components/Header.vue";
 import Layout from "@/components/Layout.vue";
+import Resume from "@/components/Resume.vue";
+import Movements from "@/components/Movements.vue";
 </script>
