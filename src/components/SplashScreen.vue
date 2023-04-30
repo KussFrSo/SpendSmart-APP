9f7a663993209a661dfef241a6754195f611a024
KussFrSo <marcfranch.fh@gmail.com>
2023-04-30 17:46:45 +0200

    [Task] SplashScreen created

---
+++ src/components/SplashScreen.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="splashscreen">
+    <img src="@/assets/icon-logo.svg" alt="LOGO" />
+    <h1><span> Spend</span>Smart</h1>
+  </div>
+</template>
+
+<style scoped>
+.splashscreen {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+}
+h1 {
+  color: var(--brand-blue);
+}
+h1 span {
+  color: var(--brand-green);
+}
+</style>
